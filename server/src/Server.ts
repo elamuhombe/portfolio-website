d9e2af6b611bba0e02177ec74515f4c3f26d4cc6
Elaine Muhombe <emuhombe@gmail.com>
2024-06-23 20:20:57 +0300

    chore: Modify the port number to 5200

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -8,7 +8,7 @@ import sendEmail from './services/SendEmail';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5200;
 
 // Use CORS middleware
 app.use(cors());
